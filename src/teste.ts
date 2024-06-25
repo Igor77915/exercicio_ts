aa52ad8a6d4118bb4dc643d84649adc39f37c6b8
igor de sousa <igorde1999sousa@gmail.com>
2024-06-25 19:19:38 -0300

    finalizando exercicio

---
+++ src/teste.ts
@@ -0,0 +1,16 @@
+function multiplcaNumero(num1:number, num2:number): number{
+    return num1 * num2
+}
+
+const resultadoMultiplicacao = multiplcaNumero(4, 5);
+console.log(resultadoMultiplicacao);
+
+
+
+
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+const saudacao = saudar("Igor");
+console.log(saudacao);
